57fc99db154503edd7f45d7fa569b6b25ae7e6ca
hafizihamid <mohamadhafizihamid@gmail.com>
2022-03-16 23:27:05 +0800

    Update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./authentication/login.component";
+import { ForgotPasswordComponent } from "./authentication/forgot-password/forgot-password.component";
 
 @NgModule({
-    declarations: [],
+    declarations: [LoginComponent, ForgotPasswordComponent],
     imports: [CommonModule],
 })
 export class CoreModule {}
