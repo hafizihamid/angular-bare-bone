d11b4c85c8d505feb5d4298f03469049fae38dbd
hafizihamid <mohamadhafizihamid@gmail.com>
2022-03-18 22:57:52 +0800

    Fix login component path

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { LoginComponent } from "./authentication/login.component";
 import { ForgotPasswordComponent } from "./authentication/forgot-password/forgot-password.component";
+import { LoginComponent } from "./authentication/login/login.component";
 
 @NgModule({
     declarations: [LoginComponent, ForgotPasswordComponent],
