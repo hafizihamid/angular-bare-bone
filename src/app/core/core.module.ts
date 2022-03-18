b4252fe95ae802ada504d94eed46a5bf40c50067
hafizihamid <mohamadhafizihamid@gmail.com>
2022-03-18 22:59:41 +0800

    Add new component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ForgotPasswordComponent } from "./authentication/forgot-password/forgot-password.component";
 import { LoginComponent } from "./authentication/login/login.component";
+import { ResetPasswordComponent } from "./authentication/reset-password/reset-password.component";
+import { SetPasswordComponent } from "./authentication/set-password/set-password.component";
 
 @NgModule({
-    declarations: [LoginComponent, ForgotPasswordComponent],
+    declarations: [
+        LoginComponent,
+        ForgotPasswordComponent,
+        ResetPasswordComponent,
+        SetPasswordComponent,
+    ],
     imports: [CommonModule],
 })
 export class CoreModule {}
