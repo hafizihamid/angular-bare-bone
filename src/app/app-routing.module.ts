b729380c6c9834c05bab53d12b9d4cb79ca302da
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-03-31 23:19:53 +0800

    FIx code styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
     {
         path: "login",
-        loadChildren: () => import("./core/core.module").then(m => m.CoreModule),
+        loadChildren: () =>
+            import("./core/core.module").then(m => m.CoreModule),
     },
 ];
 
