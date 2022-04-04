d27ada49cdc67e798bf3003d7ac1a37383bc5880
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-04-04 23:07:46 +0800

    Add validation on login form

--- src/app/core/authentication/login/login.component.ts
+++ src/app/core/authentication/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { User } from "src/app/shared/models/user";
 
 @Component({
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     isSubmit = false;
     user: User;
+    emailPattern = "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$";
 
     constructor(private formBuilder: FormBuilder) {}
 
@@ -19,6 +20,21 @@ export class LoginComponent implements OnInit {
     }
 
     login() {
-        this.loginForm = this.formBuilder.group({});
+        this.loginForm = this.formBuilder.group({
+            emailAddress: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
+            password: ["", [Validators.required]],
+            rememberMe: [false, Validators.requiredTrue],
+        });
+    }
+
+    get formControl() {
+        return this.loginForm.controls;
+    }
+
+    onLogin(): void {
+        if (this.loginForm.invalid) {
+            return;
+        } else {
+        }
     }
 }
