68615bb3870a8ce14dbecf125a0568590d9c3f0e
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-07-21 18:08:10 +0800

    Update user.ts

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,4 +1,18 @@
 export interface User {
+    id: number;
+    name: string;
     email: string;
-    password: string;
+    email_verified_at?: any;
+    created_at: Date;
+    updated_at: Date;
+    is_disabled: number;
+    blocked_until?: any;
+    tries: number;
+    roles: string[];
+    permissions: string[];
+}
+
+export interface UserData {
+    user: User;
+    accessToken: string;
 }
